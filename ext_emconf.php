7e3d2ea0a95ba6f0838ffc00eb5370e691724a08
Oliver Klee <typo3-coding@oliverklee.de>
2022-03-24 13:10:05 +0100

    [FEATURE] Add package and extension configuration files

---
+++ ext_emconf.php
@@ -0,0 +1,34 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Extra fields for TYPO3 frontend users',
+    'description' => 'This TYPO3 extension adds additional common fields to FE users and provides Extbase models for this.',
+    'version' => '2.0.0',
+    'category' => 'misc',
+    'constraints' => [
+        'depends' => [
+            'php' => '7.2.0-8.1.99',
+            'typo3' => '9.5.16-10.4.99',
+            'extbase' => '9.5.0-10.4.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+    'state' => 'stable',
+    'clearCacheOnLoad' => true,
+    'author' => 'Oliver Klee',
+    'author_email' => 'typo3-coding@oliverklee.de',
+    'author_company' => 'oliverklee.de',
+    'autoload' => [
+        'psr-4' => [
+            'OliverKlee\\FeUserExtraFields\\' => 'Classes/',
+        ],
+    ],
+    'autoload-dev' => [
+        'psr-4' => [
+            'OliverKlee\\FeUserExtraFields\\Tests\\' => 'Tests/',
+        ],
+    ],
+];
