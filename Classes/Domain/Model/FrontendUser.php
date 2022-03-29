5734549a9d4fbd2c0898f6e6ff8d077f3c5da15e
Oliver Klee <typo3-coding@oliverklee.de>
2022-03-29 17:25:26 +0200

    [FEATURE] Copy scalar fields from the core (#14)

--- Classes/Domain/Model/FrontendUser.php
+++ Classes/Domain/Model/FrontendUser.php
@@ -11,4 +11,279 @@ use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
  */
 class FrontendUser extends AbstractEntity
 {
+    /**
+     * @var string
+     */
+    protected $username = '';
+
+    /**
+     * @var string
+     */
+    protected $password = '';
+
+    /**
+     * @var string
+     */
+    protected $name = '';
+
+    /**
+     * @var string
+     */
+    protected $firstName = '';
+
+    /**
+     * @var string
+     */
+    protected $middleName = '';
+
+    /**
+     * @var string
+     */
+    protected $lastName = '';
+
+    /**
+     * @var string
+     */
+    protected $address = '';
+
+    /**
+     * @var string
+     */
+    protected $telephone = '';
+
+    /**
+     * @var string
+     */
+    protected $fax = '';
+
+    /**
+     * @var string
+     */
+    protected $email = '';
+
+    /**
+     * @var string
+     */
+    protected $lockToDomain = '';
+
+    /**
+     * @var string
+     */
+    protected $title = '';
+
+    /**
+     * @var string
+     */
+    protected $zip = '';
+
+    /**
+     * @var string
+     */
+    protected $city = '';
+
+    /**
+     * @var string
+     */
+    protected $country = '';
+
+    /**
+     * @var string
+     */
+    protected $www = '';
+
+    /**
+     * @var string
+     */
+    protected $company = '';
+
+    /**
+     * @var \DateTime|null
+     */
+    protected $lastlogin;
+
+    public function __construct(string $username = '', string $password = '')
+    {
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getMiddleName(): string
+    {
+        return $this->middleName;
+    }
+
+    public function setMiddleName(string $middleName): void
+    {
+        $this->middleName = $middleName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): void
+    {
+        $this->address = $address;
+    }
+
+    public function getTelephone(): string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): void
+    {
+        $this->telephone = $telephone;
+    }
+
+    public function getFax(): string
+    {
+        return $this->fax;
+    }
+
+    public function setFax(string $fax): void
+    {
+        $this->fax = $fax;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getLockToDomain(): string
+    {
+        return $this->lockToDomain;
+    }
+
+    public function setLockToDomain(string $lockToDomain): void
+    {
+        $this->lockToDomain = $lockToDomain;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getZip(): string
+    {
+        return $this->zip;
+    }
+
+    public function setZip(string $zip): void
+    {
+        $this->zip = $zip;
+    }
+
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): void
+    {
+        $this->city = $city;
+    }
+
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): void
+    {
+        $this->country = $country;
+    }
+
+    public function getWww(): string
+    {
+        return $this->www;
+    }
+
+    public function setWww(string $www): void
+    {
+        $this->www = $www;
+    }
+
+    public function getCompany(): string
+    {
+        return $this->company;
+    }
+
+    public function setCompany(string $company): void
+    {
+        $this->company = $company;
+    }
+
+    public function getLastlogin(): ?\DateTime
+    {
+        return $this->lastlogin;
+    }
+
+    public function setLastlogin(\DateTime $lastlogin): void
+    {
+        $this->lastlogin = $lastlogin;
+    }
 }
