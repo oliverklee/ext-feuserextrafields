fd0c5af61cf6b5460a9a44e0e4bf289165cfbbdb
Oliver Klee <typo3-coding@oliverklee.de>
2022-06-10 19:29:44 +0200

    [FEATURE] Add length validation to the model properties
    The maximum lengths match the column definitions in the database.
    
    Fixes #82

--- Classes/Domain/Model/FrontendUserGroup.php
+++ Classes/Domain/Model/FrontendUserGroup.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace OliverKlee\FeUserExtraFields\Domain\Model;
 
+use TYPO3\CMS\Extbase\Annotation as Extbase;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 
@@ -14,16 +15,19 @@ class FrontendUserGroup extends AbstractEntity
 {
     /**
      * @var string
+     * @Extbase\Validate("StringLength", options={"maximum": 50})
      */
     protected $title = '';
 
     /**
      * @var string
+     * @Extbase\Validate("StringLength", options={"maximum": 50})
      */
     protected $lockToDomain = '';
 
     /**
      * @var string
+     * @Extbase\Validate("StringLength", options={"maximum": 65535})
      */
     protected $description = '';
 
