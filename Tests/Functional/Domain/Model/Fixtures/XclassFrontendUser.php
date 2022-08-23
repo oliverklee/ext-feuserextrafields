e63586038fc36165633dec6e2627aab283873d84
Oliver Klee <typo3-coding@oliverklee.de>
2022-08-23 18:13:10 +0200

    [FEATURE] Add tests that the models can be XCLASSed
    Fixes #127

---
+++ Tests/Functional/Domain/Model/Fixtures/XclassFrontendUser.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\FeUserExtraFields\Tests\Functional\Domain\Model\Fixtures;
+
+use OliverKlee\FeUserExtraFields\Domain\Model\FrontendUser;
+
+final class XclassFrontendUser extends FrontendUser
+{
+}
---
+++ Tests/Functional/Domain/Model/Fixtures/XclassFrontendUserGroup.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\FeUserExtraFields\Tests\Functional\Domain\Model\Fixtures;
+
+use OliverKlee\FeUserExtraFields\Domain\Model\FrontendUserGroup;
+
+final class XclassFrontendUserGroup extends FrontendUserGroup
+{
+}
