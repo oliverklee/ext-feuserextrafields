41de802523a0e185221b8fe887082d6d8d09032c
Oliver Klee <typo3-coding@oliverklee.de>
2022-08-25 13:26:43 +0200

    [FEATURE] Add support for PHP 8.2 (#158)
    Fixes #126

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'misc',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-8.1.99',
+            'php' => '7.2.0-8.2.99',
             'typo3' => '10.4.1-11.5.99',
             'extbase' => '10.4.6-11.5.99',
         ],
