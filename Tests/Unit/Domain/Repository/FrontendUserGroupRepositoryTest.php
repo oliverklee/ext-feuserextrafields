fca9d1bc52b4aef4554a2c09fc90217de249b770
Oliver Klee <typo3-coding@oliverklee.de>
2023-11-03 13:14:23 +0100

    [TASK] Drop the stub ObjectManager (#402)
    Fixes #152

--- Tests/Unit/Domain/Repository/FrontendUserGroupRepositoryTest.php
+++ Tests/Unit/Domain/Repository/FrontendUserGroupRepositoryTest.php
@@ -6,7 +6,7 @@ namespace OliverKlee\FeUserExtraFields\Tests\Unit\Domain\Repository;
 
 use OliverKlee\FeUserExtraFields\Domain\Repository\DirectPersistInterface;
 use OliverKlee\FeUserExtraFields\Domain\Repository\FrontendUserGroupRepository;
-use OliverKlee\FeUserExtraFields\Tests\Unit\Fixtures\StubObjectManager;
+use TYPO3\CMS\Extbase\Object\ObjectManagerInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 use TYPO3\CMS\Extbase\Persistence\RepositoryInterface;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
@@ -25,7 +25,8 @@ final class FrontendUserGroupRepositoryTest extends UnitTestCase
     {
         parent::setUp();
 
-        $this->subject = new FrontendUserGroupRepository(new StubObjectManager());
+        $objectManagerStub = $this->createStub(ObjectManagerInterface::class);
+        $this->subject = new FrontendUserGroupRepository($objectManagerStub);
     }
 
     /**
