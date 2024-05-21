a00eddfe488e7a0b71e8162a6d71ce2856571824
Oliver Klee <typo3-coding@oliverklee.de>
2024-05-21 15:39:57 +0200

    [FEATURE] Add `FrontendUser.privacyDateOfAcceptance`
    Fixes #615

--- ext_tables.sql
+++ ext_tables.sql
@@ -7,6 +7,7 @@ CREATE TABLE fe_users (
 	date_of_birth int(11) DEFAULT '0' NOT NULL,
 	zone varchar(45) DEFAULT '' NOT NULL,
 	privacy tinyint(1) unsigned DEFAULT '0' NOT NULL,
+	privacy_date_of_acceptance int(11) unsigned DEFAULT '0' NOT NULL,
 	terms_acknowledged tinyint(1) unsigned DEFAULT '0' NOT NULL,
 	terms_date_of_acceptance int(11) unsigned DEFAULT '0' NOT NULL,
 	status tinyint(1) unsigned DEFAULT '0' NOT NULL,
