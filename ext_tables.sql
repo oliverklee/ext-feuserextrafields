fbc081fddf49c7dc23709e4780c714983b7bb0ea
Oliver Klee <typo3-coding@oliverklee.de>
2025-07-30 12:11:06 +0200

    [CLEANUP] Autoformat the SQL file (#823)

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,15 +2,15 @@
 # Table structure for table 'fe_users'
 #
 CREATE TABLE fe_users (
-	full_salutation varchar(255) DEFAULT '' NOT NULL,
-	gender tinyint(2) unsigned DEFAULT '99' NOT NULL,
-	date_of_birth int(11) DEFAULT '0' NOT NULL,
-	zone varchar(45) DEFAULT '' NOT NULL,
-	privacy tinyint(1) unsigned DEFAULT '0' NOT NULL,
+	full_salutation            varchar(255) DEFAULT '' NOT NULL,
+	gender                     tinyint(2) unsigned DEFAULT '99' NOT NULL,
+	date_of_birth              int(11) DEFAULT '0' NOT NULL,
+	zone                       varchar(45)  DEFAULT '' NOT NULL,
+	privacy                    tinyint(1) unsigned DEFAULT '0' NOT NULL,
 	privacy_date_of_acceptance int(11) unsigned DEFAULT '0' NOT NULL,
-	terms_acknowledged tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	terms_date_of_acceptance int(11) unsigned DEFAULT '0' NOT NULL,
-	status tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	comments text,
-	vat_in varchar(15) DEFAULT '' NOT NULL
+	terms_acknowledged         tinyint(1) unsigned DEFAULT '0' NOT NULL,
+	terms_date_of_acceptance   int(11) unsigned DEFAULT '0' NOT NULL,
+	status                     tinyint(1) unsigned DEFAULT '0' NOT NULL,
+	comments                   text,
+	vat_in                     varchar(15)  DEFAULT '' NOT NULL
 );
